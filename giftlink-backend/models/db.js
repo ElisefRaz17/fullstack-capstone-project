faa01b6ce3590af11644bd28f5965fa44973ff02
Elise Frazier <elisefraz11@gmail.com>
2026-08-26 18:05:12 -0400

    completed lab 2

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 
+
 // MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
 
@@ -13,16 +14,21 @@ async function connectToDatabase() {
         return dbInstance
     };
 
-    const client = new MongoClient(url);      
-
+    if(!url){
+        throw new Error('MONGO_URL environment variable is not defined. Please check your .env file and ensure it contains a valid MongoDB connection string.')
+    }
     // Task 1: Connect to MongoDB
-    // {{insert code}}
-
+   try{
+    const client = new MongoClient(url)
+    await client.connect();
     // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
-
+    dbInstance = client.db(dbName);
+    console.log(`Connected to database: ${dbInstance.databaseName}`)
     // Task 3: Return database instance
-    // {{insert code}}
+    return dbInstance
+   }catch(err){
+    throw err;
+   }
 }
 
 module.exports = connectToDatabase;
